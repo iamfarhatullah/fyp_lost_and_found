b0cccbbfa76bc2eae18eb2d8a6f54d0538d4c2e3
farhat <iamfarhatullah@gmail.com>
2025-06-29 15:20:29 +0500

    removed dev info

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -40,5 +40,4 @@ require_once('sess_auth.php');
     var _base_url_ = '<?php echo base_url ?>';
   </script>
   <script src="<?php echo base_url ?>dist/js/script.js"></script>
-  <?php echo html_entity_decode($_settings->load_data()); ?>
 </head>
